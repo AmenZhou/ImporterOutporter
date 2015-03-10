28a7c13f3596aad9b35bcd54857640f018d84d95
AmenZhou <chou.amen@gmail.com>
2015-03-10 20:30:51 +0000

    move csv parse to a seperate module

--- product.rb
+++ product.rb
@@ -1,6 +1,8 @@
 class Product
   require 'byebug'
   require_relative "./string"
+  require_relative "./csv_parse"
+  extend CsvParse
 
   attr_accessor :item_id, :description, :price, :cost, :price_type, :quantity_on_hand,
     :modifier_1_name, :modifier_1_price, :modifier_2_name, :modifier_2_price, :modifier_3_name, :modifier_3_price
@@ -11,25 +13,13 @@ class Product
     end
   end
 
+  # use different parser for different file type
+  # add new parser to extend this function
   def self.import_data path, file_type
     if file_type == :csv
-      generate_params_by_csv(path).each { |params| new(params) }
+      csv_parse(path).each { |params| new(params) }
     elsif file_type == :xls
       #generate_params_by_xls
-    end
-  end
-
-  def self.generate_params_by_csv path
-    lines = File.readlines(path)
-    single_record = {}
-    titles = lines.shift.gsub("\n", '').split(',').map(&:underscore)
-
-    lines.map do |row|
-      data = row.gsub("\n", '').split(',')
-      titles.each do |title|
-        single_record[title] = data.shift
-      end
-      single_record
     end
   end
 end
