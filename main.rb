8535e195e04e16ed45cb0817a6cbf1869a99d9b8
AmenZhou <chou.amen@gmail.com>
2015-03-10 22:56:14 +0000

    move import_data outside product class

--- main.rb
+++ main.rb
@@ -1,6 +1,18 @@
 #!/usr/bin/env ruby
 
 require_relative "./product"
+require_relative "./csv_parse"
+include CsvParse
 
-Product.import_data("/home/action/input.csv", :csv)
+# use different parser for different file type
+# add new parser to extend this function
+def import_data path, file_type
+  if file_type == :csv
+    csv_parse(path).each { |params| Product.new(params).save }
+  elsif file_type == :xls
+    #xls_parse(path)
+  end
+end
+
+import_data("/home/action/input.csv", :csv)
 p Product._to_json
--- product.rb
+++ product.rb
@@ -1,9 +1,6 @@
 class Product
-  require 'byebug'
   require_relative "./string"
-  require_relative "./csv_parse"
   require 'json'
-  extend CsvParse
 
   attr_accessor :item_id, :description, :price, :cost, :price_type, :quantity_on_hand,
     :modifier_1_name, :modifier_1_price, :modifier_2_name, :modifier_2_price, :modifier_3_name, :modifier_3_price
@@ -12,16 +9,6 @@ class Product
   def initialize params = {}
     params.each do |k, v|
       self.send("#{k}=", v)
-    end
-  end
-
-  # use different parser for different file type
-  # add new parser to extend this function
-  def self.import_data path, file_type
-    if file_type == :csv
-      csv_parse(path).each { |params| new(params).save }
-    elsif file_type == :xls
-      #generate_params_by_xls
     end
   end
 
@@ -54,7 +41,7 @@ class Product
     to_hash.to_json
   end
 
-  protected
+  private
 
   def self.before_save callback
     @@before_save = callback
